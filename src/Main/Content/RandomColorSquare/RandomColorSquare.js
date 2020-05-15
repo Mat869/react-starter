b133121790b70c75439282c3464ef22377c0e18c
M@T <matteo.rosadini.91@gmail.com>
2020-05-15 14:32:54 +0300

    slide

--- src/Main/Content/RandomColorSquare/RandomColorSquare.js
+++ src/Main/Content/RandomColorSquare/RandomColorSquare.js
@@ -2,8 +2,35 @@ import React from 'react';
 import './RandomColorSquare.css';
 
 class RandomColorSquare extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: 'red'
+        }
+    }
+
+    handleClick() {
+
+        let x = Math.floor(Math.random() * 256);
+        let y = Math.floor(Math.random() * 256);
+        let z = Math.floor(Math.random() * 256);
+
+        let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+        this.setState({
+            color: bgColor
+        });
+    }
+
 	render() {
-		return (<div className="RandomColorSquare"></div>);
+        return (<div 
+            className="RandomColorSquare"
+            style={{backgroundColor: this.state.color}}
+            onClick={this.handleClick.bind(this)}
+        >
+        </div>);
 	}
 }
 
